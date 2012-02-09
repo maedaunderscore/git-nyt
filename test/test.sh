7b061fce894f2d4972b8f4ae4b5e2cb6d0c8a0c0
maeda_ <clouds.across.the.moon@gmail.com>
2012-02-09 23:33:59 +0900

    do so many thing

--- test/test.sh
+++ test/test.sh
@@ -21,9 +21,9 @@ testSimpleCommit()
 {
     echo b > b.txt
     git add b.txt
-    git nyt commit -m 'hoge'
+    git nyt commit -m 'hoge fuga'
     assertEquals $? 0
-    assertEquals 'hoge' $(git log -1 --format=%s)
+    assertEquals 'hoge fuga' "$(git log -1 --format=%s)"
 }
 
 testSetBase()
@@ -32,24 +32,23 @@ testSetBase()
     assertEquals 1 $?
     echo b > b.txt
     git add b.txt
-    git nyt commit -m 'hoge'
-    assertEquals $(git log --format=%H) $(git config nyt.base)
+    git nyt commit -m 'hoge fuga'
+    assertEquals "$(git log --format=%H -1 HEAD~1)" "$(git config nyt.base)"
     echo c > b.txt
     git add b.txt
-    git nyt commit -m 'hoge'
-    assertEquals $(git log --format=%H) $(git config nyt.base)
+    git nyt commit -m 'foo bar'
+    assertEquals "$(git log --format=%H -1 HEAD~2)" "$(git config nyt.base)"
 }
 
 testKeepBase()
 {
     echo b > b.txt
     git add b.txt
-    git nyt commit -m 'hoge'
+    git nyt commit -m 'hoge hoge'
     base=$(git config nyt.base)
-    assertEquals $(git log -2 --format=%H) $base
     echo c >> b.txt
-    git nyt commit -a -m 'fuga'
-    assertEquals $base $(git config nyt.base)
+    git nyt commit -a -m 'fuga fuga'
+    assertEquals $base "$(git config nyt.base)"
 }
 
 testClearBase()
@@ -58,7 +57,6 @@ testClearBase()
     git add b.txt
     git nyt commit -m 'hoge'
     base=$(git config nyt.base)
-    assertEquals $(git log -2 --format=%H) $base
     echo c >> b.txt
     git nyt commit -a -m 'fuga'
     assertEquals $base $(git config nyt.base)
@@ -72,7 +70,7 @@ testList()
     for i in {1..5}; do 
 	echo $i > $i.txt
 	git add $i.txt
-	git nyt commit -m "commit:$i"
+	git nyt commit -m "commit: $i"
     done
     assertEquals 5 $(git nyt list | wc -l)
 }
@@ -82,7 +80,7 @@ testDiff()
     for i in {1..5}; do 
 	echo $i > $i.txt
 	git add $i.txt
-	git nyt commit -m "commit:$i"
+	git nyt commit -m "commit: $i"
     done
     assertEquals "1.txt
 2.txt
@@ -96,53 +94,36 @@ testFixup()
     for i in {1..5}; do 
 	echo $i > $i.txt
 	git add $i.txt
-	git nyt commit -m "commit:$i"
+	git nyt commit -m "commit: $i"
     done
-    git nyt fixup -m 'one'
+    git nyt fixup -m 'commit: one'
     assertEquals 2 $(git log --pretty=oneline | wc -l)
-    assertEquals 'one' $(git log -1 --format=%s)
+    assertEquals ' commit: one' "$(git log -1 --format=%s)"
 
     for i in {6..10}; do 
 	echo $i > $i.txt
 	git add $i.txt
-	git nyt commit -m "commit:$i"
+	git nyt commit -m "commit: $i"
     done
-    git nyt fixup -m 'two'
+    git nyt fixup -m 'commit: two'
     assertEquals 3 $(git log --pretty=oneline | wc -l)
-    assertEquals 'two' $(git log -1 --format=%s)
+    assertEquals ' commit: two' "$(git log -1 --format=%s)"
 
-}
-
-testFixupPart()
-{
-    for i in {1..5}; do 
-	echo $i > $i.txt
-	git add $i.txt
-	git nyt commit -m "commit:$i"
-    done
-    git nyt list | tail -n 3 | git nyt fixup-part -m 'hoge'
-    assertEquals 'commit:2' $(git log --pretty=%s -1)
-    assertEquals 'commit:1' $(git log --pretty=%s -2| tail -n1)
-    assertEquals 'hoge' $(git log --pretty=%s -3| tail -n1)
-    assertEquals '3.txt
-4.txt
-5.txt' "$(git diff HEAD~2 HEAD~3 --name-only)"
-    assertEquals 'first commit' "$(git log --pretty=%s -4| tail -n1)"
 }
 
 testBleis()
 {
     echo 1 > 1.txt
     git add 1.txt
-    git nyt bleis -m 'commit:1'
+    git nyt bleis -m 'commit: 1'
     assertEquals 1 $(git log --pretty=oneline | wc -l)
     echo 2 > 1.txt
     git add 1.txt
-    git nyt bleis -m 'commit:2'
+    git nyt bleis -m 'commit: 2'
     assertEquals 2 $(git log --pretty=oneline | wc -l)
     echo 3 > 2.txt
     git add 2.txt
-    git nyt bleis -m 'commit:3'
+    git nyt bleis -m 'commit: 3'
     assertEquals 2 $(git log --pretty=oneline | wc -l)
 }
 
---
+++ test/testFixPart.sh
@@ -0,0 +1,81 @@
+#! /bin/sh
+
+setUp()
+{
+    mkdir testunit_work
+    cd testunit_work
+    export PATH=../..:$PATH
+    git init
+    echo a > a.txt
+    git add a.txt
+    git commit -m 'first commit'
+}
+
+tearDown()
+{
+    cd ..
+    rm -rf testunit_work
+}
+testFixupPart()
+{
+    for i in {1..5}; do 
+	echo $i > $i.txt
+	git add $i.txt
+	git nyt commit -m "commit: $i"
+    done
+    git nyt list | tail -n 3 | git nyt fixup-part -m 'hoge hoge'
+    assertEquals 'commit: 2' "$(git log --pretty=%s -1)"
+    assertEquals 'commit: 1' "$(git log --pretty=%s -2| tail -n1)"
+    assertEquals ' hoge hoge' "$(git log --pretty=%s -3| tail -n1)"
+    assertEquals '3.txt
+4.txt
+5.txt' "$(git diff HEAD~2 HEAD~3 --name-only)"
+    assertEquals 'first commit' "$(git log --pretty=%s -4| tail -n1)"
+}
+
+alias git-now='git nyt commit -m "[from now] $(date) $(echo $(git diff --pretty=oneline --name-only --cached))"'
+testFixupByFilename()
+{
+    for i in {1..5}; do 
+	echo $i > $i.txt
+	git add $i.txt
+	git-now
+    done
+    git nyt list | grep -e '3.txt$' | git nyt fixup-part -m 'only 3'
+    assertEquals ' only 3' "$(git log --pretty=%s -5| tail -n1)"
+    assertEquals '5.txt' "$(git diff HEAD~1 HEAD   --name-only)"
+    assertEquals '4.txt' "$(git diff HEAD~2 HEAD~1 --name-only)"
+    assertEquals '2.txt' "$(git diff HEAD~3 HEAD~2 --name-only)"
+    assertEquals '1.txt' "$(git diff HEAD~4 HEAD~3 --name-only)"
+    assertEquals '3.txt' "$(git diff HEAD~5 HEAD~4 --name-only)"
+    assertEquals 'first commit' "$(git log --pretty=%s -6| tail -n1)"
+}
+
+testFixupByTicket()
+{
+    echo a >> a.txt
+    git add .
+    git nyt commit -m 'refs #1'
+    echo b >> b.txt
+    git add .
+    git nyt commit -a -m 'refs #2'
+    echo c >> c.txt
+    git add .
+    git nyt commit -a -m 'refs #2'
+    echo d >> d.txt
+    git add .
+    git nyt commit -a -m 'refs #3'
+    echo e >> e.txt
+    git add .
+    git nyt commit -a -m 'refs #2'
+    git nyt list | grep 'refs #2' | git nyt fixup-part -m 'refs #2'
+    assertEquals ' refs #2' "$(git log --pretty=%s -3| tail -n1)"
+    assertEquals 'd.txt' "$(git diff HEAD~1 HEAD   --name-only)"
+    assertEquals 'a.txt' "$(git diff HEAD~2 HEAD~1 --name-only)"
+    assertEquals 'b.txt
+c.txt
+e.txt' "$(git diff HEAD~3 HEAD~2 --name-only)"
+    assertEquals 4 $(git log --pretty=oneline | wc -l)
+}
+
+. ../shunit2-2.1.6/src/shunit2
