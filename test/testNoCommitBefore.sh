29af7003782635feb8472a310940ff62c40fd929
maeda_ <clouds.across.the.moon@gmail.com>
2012-02-16 01:07:00 +0900

    main関数を作成

--- test/testNoCommitBefore.sh
+++ test/testNoCommitBefore.sh
@@ -17,81 +17,85 @@ tearDown()
     rm -rf testunit_work
 }
 
-#test__simple_commit()
-#{
-#    echo b > b.txt
-#    git nyt add b.txt
-#    assertEquals $? 0
-#    assertEquals "" "$(git nyt base)"
-#    git nyt commit -m 'hoge fuga'
-#    assertEquals $? 0
-#    assertEquals 'hoge fuga' "$(git log -1 --format=%s)"
-#    assertEquals "" "$(git nyt base)"
-#}
-#
-#test__set_base()
-#{
-#    git config nyt.base
-#    assertEquals 1 $?
-#    echo b > b.txt
-#    git add b.txt
-#    git nyt commit -m 'hoge fuga'
-#    assertEquals "" "$(git config nyt.base)"
-#    echo c > b.txt
-#    git add b.txt
-#    git nyt commit -m 'foo bar'
-#    assertEquals "" "$(git config nyt.base)"
-#}
-#
-#test__clear_base()
-#{
-#    echo b > b.txt
-#    git add b.txt
-#    git nyt commit -m 'hoge'
-#    base=$(git config nyt.base)
-#    echo c >> b.txt
-#    git nyt commit -a -m 'fuga'
-#    assertEquals "$base" "$(git config nyt.base)"
-#    git nyt clear
-#    git config nyt.base
-#    assertEquals $? 1
-#}
-#
-#OneToFive="1 2 3 4 5"
-#SixToTen="6 7 8 9 10"
-#
-#test__list()
-#{
-#    for i in $OneToFive; do 
-#	echo $i > $i.txt
-#	git add $i.txt
-#	git nyt commit -m "commit: $i"
-#    done
-#    assertEquals 5 $(git nyt list | wc -l)
-#}
-#
-#test__diff()
-#{
-#    for i in $OneToFive; do 
-#	echo $i > $i.txt
-#	git add $i.txt
-#	git nyt commit -m "commit: $i"
-#    done
-#    git nyt diff --name-only
-#    assertNotEquals "0" "$?"
-#}
-#
-#test__fixup()
-#{
-#    for i in $OneToFive; do 
-#	echo $i > "file $i.txt"
-#	git add "file $i.txt"
-#	git nyt commit -m "commit: $i"
-#    done
-#    git nyt fixup -m 'commit: one'
-#    assertNotEquals "0" "$?"
-#}
-#
+test__simple_commit()
+{
+    echo b > b.txt
+    git nyt add b.txt
+    assertEquals 0 $?
+    assertEquals "" "$(git nyt base)"
+    git nyt commit -m 'hoge fuga'
+    assertEquals 0 $?
+    assertEquals 'hoge fuga' "$(git log -1 --format=%s)"
+    assertEquals "" "$(git nyt base)"
+}
+
+
+test__clear_base()
+{
+    assertEquals "" "$(git config nyt.base)"
+    echo b > b.txt
+    git add b.txt
+    git nyt commit -m 'hoge'
+    assertEquals "" "$(git config nyt.base)"
+    echo c >> b.txt
+    git nyt commit -a -m 'fuga'
+    assertEquals "" "$(git config nyt.base)"
+    git nyt clear
+    git config nyt.base
+    assertEquals $? 1
+}
+
+OneToFive="1 2 3 4 5"
+SixToTen="6 7 8 9 10"
+
+test__list()
+{
+    for i in $OneToFive; do 
+	echo $i > $i.txt
+	git add $i.txt
+	git nyt commit -m "commit: $i"
+    done
+    assertEquals 5 $(git nyt list | wc -l)
+}
+
+test__diff()
+{
+    for i in $OneToFive; do 
+	echo $i > $i.txt
+	git add $i.txt
+	git nyt commit -m "commit: $i"
+    done
+    git nyt diff --name-only
+    assertNotEquals "0" "$?"
+}
+
+test__fixup()
+{
+    for i in $OneToFive; do 
+	echo $i > "file $i.txt"
+	git add "file $i.txt"
+	git nyt commit -m "commit: $i"
+    done
+    git nyt fixup -m 'commit: one'
+    assertEquals "0" "$?"
+}
+
+test__fixup_part()
+{
+    for i in $OneToFive; do 
+	echo $i > $i.txt
+	git add $i.txt
+	git nyt commit -m "commit: $i"
+    done
+    git nyt list | tail -n 3 | git nyt fixup-part -m 'hoge hoge'
+    assertEquals 'commit: 2' "$(git log --pretty=%s -1)"
+    assertEquals 'commit: 1' "$(git log --pretty=%s -2| tail -n1)"
+    assertEquals 'hoge hoge' "$(git log --pretty=%s -3| tail -n1)"
+    assertEquals '3.txt
+4.txt
+5.txt' "$(git diff HEAD~2 HEAD~3 --name-only)"
+}
+
 #test__bleis()
 #{
 #    echo 1 > 1.txt
